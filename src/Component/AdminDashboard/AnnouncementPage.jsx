35ceb1847b1f582a4f2be74a44b012be45bf3d37
DavidAjanaku <davidajanaku46163@gmail.com>
2023-11-03 18:19:37 +0100

    done

--- src/Component/AdminDashboard/AnnouncementPage.jsx
+++ src/Component/AdminDashboard/AnnouncementPage.jsx
@@ -16,34 +16,37 @@ const AnnouncementPage = () => {
     <div className="bg-gray-100 h-[100vh]">
       <AdminHeader />
       <div className="flex">
-        <div className="bg-orange-400  text-white hidden md:block md:w-1/5 h-[100vh] p-4">
+        <div className="bg-orange-400 text-white hidden md:block md:w-1/5 h-[100vh] p-4">
           <AdminSidebar />
         </div>
 
         <div className="w-full md:w-4/5 p-4 h-[100vh] overflow-auto example">
-        <h1 className="text-2xl font-semibold mb-4">Send Announcement to All Users</h1>
+          <h1 className="text-2xl font-semibold mb-4">
+            Send Announcement to All Users
+          </h1>
 
           <Form form={form} onFinish={handleSubmit}>
-      
+            <Form.Item
+              name="target"
+              rules={[
+                { required: true, message: "Please select the target audience" },
+              ]}
+            >
+              <select className="p-2">
+                <option value="Guest">Guest</option>
+                <option value="Host">Host</option>
+                <option value="All">All</option>
+              </select>
+            </Form.Item>
             <Form.Item
               name="announcement"
-              label="Announcement"
               rules={[
                 { required: true, message: "Please enter your announcement" },
-                
               ]}
-            >    <select className="p-2">
-            <option>Guest</option>
-            <option>Host</option>
-            <option>All</option>
-
-
-          </select>
-                <br />
+            >
               <Input.TextArea rows={4} />
             </Form.Item>
             <Form.Item>
-            
               <Button type="primary" htmlType="submit">
                 Send Announcement
               </Button>
