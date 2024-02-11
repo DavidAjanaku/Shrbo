c91a9ef57739aef664b0b94d6124369aaaf6d5da
Beastly12 <otudjeodafe@gmail.com>
2024-02-11 09:46:31 +0000

    Implemented Protected Routing and added PageNotFound

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Home from "./Views/Home";
 import ListingInfoMain from "./Views/ListingInfoMain";
 import Settings from "./Views/Settings";
 import Profile from "./Component/Settings/Profile";
-import Payments from "./Component/Settings/Payments";
 import Security from "./Component/Settings/Security";
 import Chat from "./Component/Chat/Chat";
 import AccountNotifications from "./Component/Settings/ProfileCardSettings/AccountNotifications";
@@ -87,23 +86,12 @@ function App() {
         <Route path="/RequestBook" element={<RequestBook />} />
         <Route path="/Settings" element={<Settings />} />
         <Route path="/Profile" element={<Profile />} />
-        <Route path="/Payments" element={<Payments />} />
-        <Route path="/Security" element={<Security />} />
-        <Route path="/AddGovvernmentId" element={<AddGovvernmentId />} />
-        <Route
-          path="/AccountNotifications"
-          element={<AccountNotifications />}
-        />
-        <Route path="/Chat" element={<Chat />} />
-        <Route path="/Trip" element={<Trip />} />
-        <Route path="/WishList" element={<WishList />} />
-        <Route path="/WishlistsSet" element={<WishlistsSet />} />
-        <Route path="/WishlistsSet/:wishList/:id" element={<WishlistsSet />} />
-
+      
           <Route element={<Protected />} >
 
               <Route path="/Settings" element={<Settings />} />
               <Route path="/Profile" element={<Profile />} />
+              <Route path="/UsersShow" element={<UsersShow />} />
               <Route path="/Payments" element={<PaymentsTab />} />
               <Route path="/Security" element={<Security />} />
               <Route path="/AddGovvernmentId" element={<AddGovvernmentId />} />
