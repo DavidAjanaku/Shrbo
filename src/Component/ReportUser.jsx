d8a7cc9356cfcdeada06fb0f5e6232f3cc65c796
DavidAjanaku <davidajanaku46163@gmail.com>
2024-04-29 11:35:54 +0100

    "Log data sent in report user request and update textarea handling"
    This commit logs the data being sent in the Axios request for reporting a user. It also updates the handling of the additional details textarea by adding a value and onChange

--- src/Component/ReportUser.jsx
+++ src/Component/ReportUser.jsx
@@ -36,17 +36,21 @@ const ReportForm = () => {
         extrareasonforreporting: additionalDetails,
       });
       console.log("Report submitted successfully:", response.data);
+  
+      // Log the data sent in the Axios request
+      console.log("Data sent:", {
+        reasonforreporting: reportType,
+        hostorguestuser_id: id,
+        extrareasonforreporting: additionalDetails,
+      });
+  
       setLoading(false);
       notification.success({
         message: "Report Submitted",
         description: "Your report has been submitted successfully.",
       });
-      setModalVisible(false); // Close the modal after successful submission
-
-      // Handle any success behavior here, like showing a success message
     } catch (error) {
       console.error("Error submitting report:", error);
-      // Handle any error behavior here, like showing an error message
       setLoading(false);
       notification.error({
         message: "Error",
@@ -54,6 +58,7 @@ const ReportForm = () => {
       });
     }
   };
+  
   
 
 
@@ -129,13 +134,14 @@ const ReportForm = () => {
             </div>
             <div className="">
               <div className="mt-2.5">
-                <textarea
+              <textarea
                   name="message"
                   id="message"
                   rows={4}
                   placeholder="(optional) give us more details on why you think the listing is inaccurate "
-                  className="block w-full rounded-md ring-0 outline-gray-400  border px-3.5 py-2 text-gray-900 shadow-sm focus:outline-gray-400  placeholder:text-gray-400 sm:text-sm sm:leading-6"
-                  defaultValue={""}
+                  className="block w-full rounded-md ring-0 outline-gray-400 border px-3.5 py-2 text-gray-900 shadow-sm focus:outline-gray-400 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                  value={additionalDetails}
+                  onChange={(e) => setAdditionalDetails(e.target.value)}
                 />
               </div>
             </div>
